16e9677da8453b3bd573cdf922d9b2bb2e979a03
‘gonzo’ <pnw123456@icloud.com>
2021-02-17 22:31:48 +0900

    feature: #2 xml passer
    - return type change DTO

--- src/main/java/io/gonzo/middleware/web/dto/TransactionDTO.java
+++ src/main/java/io/gonzo/middleware/web/dto/TransactionDTO.java
@@ -5,18 +5,54 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionDTO {
 
+    // 거래금
     private String amount;
 
+    // 아파트
     private String apartment;
 
-    public Integer getAmount() {
-        return Integer.valueOf(amount.replace("," ,""));
+    // 연
+    private String year;
+
+    // 월
+    private String month;
+
+    // 일
+    private String day;
+
+    // 도로명
+    private String roadName;
+
+    private String buildingFirstCode;
+
+    private String buildingSecondCode;
+
+    private String buildingCityCoed;
+
+    private String buildingSerialCode;
+
+    private String buildingUndergroundCode;
+
+    private String buildingNameCode;
+
+    private String legalBuilding;
+
+    private String legalBuildingCode;
+
+    private String legalBuildingSecondCode;
+
+    private String legalBuildingCityCode;
+
+    public BigDecimal getAmount() {
+        return BigDecimal.valueOf(Long.valueOf(amount.replace(",", "")));
     }
 
 }
