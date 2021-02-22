03e637d4b39a47fa48dcd6822873a139d0a00a2f
‘gonzo’ <pnw123456@icloud.com>
2021-02-22 19:39:02 +0900

    style: #2 아파트매매 실거래 상세 자료 API
    - Xml Utils 추가

---
+++ src/main/java/io/gonzo/middleware/utils/XmlUtils.java
@@ -0,0 +1,17 @@
+package io.gonzo.middleware.utils;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XmlUtils {
+
+    public static String getTagValue(String tag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        if (nValue == null)
+            return null;
+        return nValue.getNodeValue();
+    }
+
+}
