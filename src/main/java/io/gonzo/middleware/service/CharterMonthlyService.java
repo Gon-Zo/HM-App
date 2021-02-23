dabbd5e00bc347dec0bf6d255f2a8d7a7db9106f
‘gonzo’ <pnw123456@icloud.com>
2021-02-23 20:48:18 +0900

    feat: #3 아파트 전월세 API
    - 아파트 전월세 API 개발

---
+++ src/main/java/io/gonzo/middleware/service/CharterMonthlyService.java
@@ -0,0 +1,82 @@
+package io.gonzo.middleware.service;
+
+import io.gonzo.middleware.web.dto.CharterMonthlyDTO;
+import io.gonzo.middleware.web.dto.CharterMonthlyStoreDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.gonzo.middleware.utils.XmlUtils.getTagValue;
+
+@Service
+public class CharterMonthlyService {
+
+    @Value("${app.key}")
+    private String key;
+
+    public List<CharterMonthlyDTO> getByCharterAndMonthly(CharterMonthlyStoreDTO dto){
+
+        List<CharterMonthlyDTO> result = new ArrayList<>();
+
+        try {
+
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String url = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent";
+
+            stringBuffer.append(url)
+                    .append("?ServiceKey=")
+                    .append(key)
+                    .append("&LAWD_CD=")
+                    .append(dto.getLocalCode())
+                    .append("&DEAL_YMD=")
+                    .append(dto.getPickDate());
+
+            DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+
+            DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
+
+            Document doc = dBuilder.parse(stringBuffer.toString());
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("item");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    result.add(
+                            CharterMonthlyDTO.builder()
+                                    .amount(getTagValue("보증금액", eElement))
+                                    .apartment(getTagValue("아파트", eElement))
+                                    .monthlyRent(getTagValue("월세금액", eElement))
+                                    .pickDate(dto.getPickDate())
+                                    .build()
+                    );
+
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+
+    }
+
+}
---
+++ src/main/java/io/gonzo/middleware/web/dto/CharterMonthlyStoreDTO.java
@@ -0,0 +1,24 @@
+package io.gonzo.middleware.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static io.gonzo.middleware.utils.DtoUtils.changeToByPicker;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CharterMonthlyStoreDTO {
+
+    private String pickDate;
+
+    private String localCode;
+
+    public String getPickDate() {
+        return changeToByPicker(this.pickDate);
+    }
+
+}
