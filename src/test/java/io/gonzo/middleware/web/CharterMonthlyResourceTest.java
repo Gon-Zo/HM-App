4b8d429b3ea4a50376b19ef53155e889ec55dc26
‘gonzo’ <pnw123456@icloud.com>
2021-02-28 16:38:05 +0900

    test: #3  아파트 전월세 API
    - test case 작성

---
+++ src/test/java/io/gonzo/middleware/web/CharterMonthlyResourceTest.java
@@ -0,0 +1,17 @@
+package io.gonzo.middleware.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CharterMonthlyResourceTest {
+
+    @Test
+    @DisplayName("api_테스트")
+    public void testByCharterMonthly() {
+
+    }
+
+}
