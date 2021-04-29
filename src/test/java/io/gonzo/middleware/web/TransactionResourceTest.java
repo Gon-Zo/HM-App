d077f343b8898044c290fe30e87f6e5db6d68ac8
‘gonzo’ <pnw123456@icloud.com>
2021-04-29 22:39:42 +0900

    test: 아파트매매 실거래 상세 자료 API #2
    - 아파트 매매 실거래 상세 자료 월별 기준 test case 작성

--- src/test/java/io/gonzo/middleware/web/TransactionResourceTest.java
+++ src/test/java/io/gonzo/middleware/web/TransactionResourceTest.java
@@ -2,21 +2,35 @@ package io.gonzo.middleware.web;
 
 import io.gonzo.middleware.service.RegionService;
 import io.gonzo.middleware.service.TransactionService;
+import io.gonzo.middleware.web.dto.TransactionDTO;
 import io.gonzo.middleware.web.dto.TransactionStoreDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration()
+@AutoConfigureMockMvc
 @WebMvcTest(TransactionResource.class)
 class TransactionResourceTest {
 
@@ -33,20 +47,32 @@ class TransactionResourceTest {
     @DisplayName("파트매매 실거래 상세 [월기준]")
     public void showByTransaction() throws Exception {
 
-        // dto
+        // param
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        params.add("pickDate", "2020-01");
+
+        params.add("regionName", "서울특별시종로구");
+
+        params.add("pageNum", "100");
+
+        params.add("localCode" , "11000");
+
         TransactionStoreDTO dto = TransactionStoreDTO.builder()
-                .pickDate("2020-01")
-                .regionName("서울특별시종로구")
                 .pageNum(100)
+                .regionName("서울특별시종로구")
+                .pickDate("2020-01")
+                .localCode("11000")
                 .build();
 
-        String localCode = regionService.getByRegionCode(dto.getRegionName());
+        List<TransactionDTO> transactionList = new ArrayList<>();
 
-        dto.setLocalCode(localCode);
+        // given
+        given(transactionService.getByTransaction(dto)).willReturn(transactionList);
 
         //when
-//        final ResultActions actions = mvc.perform(get("/api/middleware/transaction")
-        final ResultActions actions = mvc.perform(get("/api/middleware/transaction?pickDate=2020-01&regionName=서울특별시종로구&pageNum=100")
+        final ResultActions actions = mvc.perform(get("/api/middleware/transaction")
+                .params(params)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print());
 
