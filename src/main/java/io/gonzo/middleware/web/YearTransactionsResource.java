74c9e294b4508953024818904727bc1747e3a7ae
‘gonzo’ <pnw123456@icloud.com>
2021-05-21 21:18:38 +0900

    refactor: web resource
    - responseEntity 추가

--- src/main/java/io/gonzo/middleware/web/YearTransactionsResource.java
+++ src/main/java/io/gonzo/middleware/web/YearTransactionsResource.java
@@ -4,6 +4,7 @@ import io.gonzo.middleware.service.YearTransactionsService;
 import io.gonzo.middleware.web.dto.YearTransactionsDTO;
 import io.gonzo.middleware.web.dto.YearTransactionsStoreDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,9 @@ public class YearTransactionsResource {
     private final YearTransactionsService service;
 
     @GetMapping("")
-    public List<YearTransactionsDTO> showByYearTransactions(YearTransactionsStoreDTO dto) {
-        return service.getByYearTransactions(dto);
+    public ResponseEntity<List<YearTransactionsDTO>> showByYearTransactions(YearTransactionsStoreDTO dto) {
+        List<YearTransactionsDTO> data = service.getByYearTransactions(dto);
+        return ResponseEntity.ok().body(data);
     }
 
 }
