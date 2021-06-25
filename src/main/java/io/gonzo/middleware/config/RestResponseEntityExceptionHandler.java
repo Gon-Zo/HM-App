b7cdd15e48344be720aa79367982ad04b4cf4d4c
‘gonzo’ <pnw123456@icloud.com>
2021-06-25 23:30:48 +0900

    feat: global exception
    - global exception develop

---
+++ src/main/java/io/gonzo/middleware/config/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,49 @@
+package io.gonzo.middleware.config;
+
+import io.gonzo.middleware.enums.ErrorCode;
+import io.gonzo.middleware.web.dto.ErrorDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.Instant;
+
+import static io.gonzo.middleware.enums.ErrorCode.*;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    protected ResponseEntity<Object> handleConflict(NullPointerException ex, WebRequest request) {
+
+        ErrorCode error = NULL_POINT_ERROR;
+
+        ErrorDTO dto = ErrorDTO.builder()
+                .code(error.getCode())
+                .message(error.getMessage())
+                .detailMessage(getStackTracePasserToString(ex))
+                .createDate(Instant.now())
+                .build();
+
+        return handleExceptionInternal(ex, dto, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    private String getStackTracePasserToString(Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+
+}
---
+++ src/main/java/io/gonzo/middleware/web/dto/ErrorDTO.java
@@ -0,0 +1,27 @@
+package io.gonzo.middleware.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+public class ErrorDTO {
+
+    private final String code;
+
+    private final String message;
+
+    private final String detailMessage;
+
+    private final Instant createDate;
+
+    @Builder
+    public ErrorDTO(String code, String message, String detailMessage, Instant createDate) {
+        this.code = code;
+        this.message = message;
+        this.detailMessage = detailMessage;
+        this.createDate = createDate;
+    }
+
+}
