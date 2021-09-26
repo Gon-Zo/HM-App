14040084014ee2545928fff039ba41f5dbac59e3
‘gonzo’ <pnw123456@icloud.com>
2021-09-26 13:36:44 +0900

    feat: check controller develop

---
+++ src/main/java/io/gonzo/middleware/web/CheckResource.java
@@ -0,0 +1,18 @@
+package io.gonzo.middleware.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/api/check")
+public class CheckResource {
+
+    @GetMapping("/state")
+    public Instant checkByState(){
+        return Instant.now();
+    }
+
+}
